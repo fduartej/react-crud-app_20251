c91adc995ebddc86728fd83fb3ae523edc9642ac
Fred Duarte <fduartej@gmail.com>
2025-05-03 16:55:15 -0500

    app tienda url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5209/api/producto"; // Replace with your actual API URL
+const API_URL = "https://apptienda-btfc.onrender.com/api/producto"; // Replace with your actual API URL
 
 export const getProducts = () => axios.get(API_URL);
 export const getProduct = (id) => axios.get(`${API_URL}/${id}`);
